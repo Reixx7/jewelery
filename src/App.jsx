015fde00a06873c2e9612d473b8672812b897c58
Reixx7 <reixx23and7@gmail.com>
2025-12-18 10:01:10 +0500

    fnvj

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Zari from './components/Zari.jsx'
-import Ezo from './Components/Ezo.jsx'  
+import Ezo from './components/Ezo.jsx'  
 
 const App = () => {
   return (
@@ -8,7 +8,7 @@ const App = () => {
       <Zari/>
 
 
-      <Ezo />
+      
     </div>
   )
 }
